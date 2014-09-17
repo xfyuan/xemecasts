54784ab7b4bac93427dc0989afca35bf5bfdc774
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2014-09-17 20:45:15 +0800

    add description comments for uploading page

--- app/views/admin/xcasts/upload_image.blade.php
+++ app/views/admin/xcasts/upload_image.blade.php
@@ -6,6 +6,7 @@
     <div class="row">
         <div class="col-md-12">
             <h3> {{$xcast->title}} </h3>
+            <p class="bg-danger text-danger">You can upload your own poster image for your video cast. Otherwise, it should be set a default poster. Recommend to use 16:10 resolution.</p>
 
             {{ Form::model($xcast, ['method' => 'POST', 'route' => ['admin.xcasts.transmit_image', $xcast->id], 'class' => 'dropzone', 'id' => 'xe-dropzone']) }}
             {{ Form::close() }}
--- app/views/admin/xcasts/upload_video.blade.php
+++ app/views/admin/xcasts/upload_video.blade.php
@@ -6,6 +6,7 @@
     <div class="row">
         <div class="col-md-12">
             <h3> {{$xcast->title}} </h3>
+            <p class="bg-danger text-danger">Only support mp4 format video now. Recommend to use 16:10 or 16:9 resolution.</p>
 
             {{ Form::model($xcast, ['method' => 'POST', 'route' => ['admin.xcasts.transmit_video', $xcast->id], 'class' => 'dropzone', 'id' => 'xe-dropzone']) }}
             {{ Form::close() }}
